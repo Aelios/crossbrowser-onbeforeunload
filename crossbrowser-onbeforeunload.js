bc1f8b09adbf7277d3c7387a2da069a8eed13700
Aelios <lefevre.nicolas88@gmail.com>
2014-05-12 13:06:41 +0200

    Add custom dialog example

--- crossbrowser-onbeforeunload.js
+++ crossbrowser-onbeforeunload.js
@@ -3,9 +3,19 @@ window.onbeforeunload = function (e) {
 	var firefox = /Firefox[\/\s](\d+)/.test(navigator.userAgent);
 	if (firefox) {
 		//Add custom dialog
+		//Firefox does not accept window.showModalDialog(), window.alert(), window.confirm(), and window.prompt() furthermore
 		var custom_div = document.createElement("div");
+        document.body.appendChild(custom_div);
+        custom_div.style.visibility = "hidden";
+        custom_div.style.color = "white";
+        custom_div.style.padding = "20px";
+        custom_div.style.backgroundColor = "red";
+		custom_div.style.position = "fixed";
+		custom_div.style.zIndex = 999;
 		custom_div.innerHTML = message; 
-		document.body.appendChild(custom_div);
+        var left = document.body.clientWidth / 2 - custom_div.clientWidth / 2;
+        custom_div.style.left = left + "px";
+        custom_div.style.visibility = "visible";        
 	}
 	return message;
 }
