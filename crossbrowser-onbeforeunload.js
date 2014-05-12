894fa5f0a8239000e7dff84941113f2c28707644
Aelios <lefevre.nicolas88@gmail.com>
2014-05-12 13:54:17 +0200

    Update crossbrowser-onbeforeunload.js

--- crossbrowser-onbeforeunload.js
+++ crossbrowser-onbeforeunload.js
@@ -5,13 +5,13 @@ window.onbeforeunload = function (e) {
 		//Add custom dialog
 		//Firefox does not accept window.showModalDialog(), window.alert(), window.confirm(), and window.prompt() furthermore
 		var dialog = document.createElement("div");
-        document.body.appendChild(dialog);
+        	document.body.appendChild(dialog);
 		dialog.id = "dialog";
-        dialog.style.visibility = "hidden";
+        	dialog.style.visibility = "hidden";
 		dialog.innerHTML = message; 
-        var left = document.body.clientWidth / 2 - dialog.clientWidth / 2;
-        dialog.style.left = left + "px";
-        dialog.style.visibility = "visible";     
+        	var left = document.body.clientWidth / 2 - dialog.clientWidth / 2;
+        	dialog.style.left = left + "px";
+		dialog.style.visibility = "visible";     
 		//tip with setTimeout
 		setTimeout(function () {
 			document.body.removeChild(document.getElementById("dialog"));
