26fe88e1e965b5cf696968e3e74b966ea7fec8c7
Aelios <lefevre.nicolas88@gmail.com>
2014-05-12 13:09:55 +0200

    Add css file

--- crossbrowser-onbeforeunload.js
+++ crossbrowser-onbeforeunload.js
@@ -7,11 +7,6 @@ window.onbeforeunload = function (e) {
 		var custom_div = document.createElement("div");
         document.body.appendChild(custom_div);
         custom_div.style.visibility = "hidden";
-        custom_div.style.color = "white";
-        custom_div.style.padding = "20px";
-        custom_div.style.backgroundColor = "red";
-		custom_div.style.position = "fixed";
-		custom_div.style.zIndex = 999;
 		custom_div.innerHTML = message; 
         var left = document.body.clientWidth / 2 - custom_div.clientWidth / 2;
         custom_div.style.left = left + "px";
