cf0119ee1e6c3304a69ad212f7914a6c312c7704
Aelios <lefevre.nicolas88@gmail.com>
2014-05-14 14:01:52 +0200

    Add shadow

--- crossbrowser-onbeforeunload.js
+++ crossbrowser-onbeforeunload.js
@@ -11,10 +11,14 @@ window.onbeforeunload = function (e) {
 		dialog.innerHTML = message; 
 		var left = document.body.clientWidth / 2 - dialog.clientWidth / 2;
 		dialog.style.left = left + "px";
-		dialog.style.visibility = "visible";     
+		dialog.style.visibility = "visible";  
+		var shadow = document.createElement("div");
+		document.body.appendChild(shadow);
+		shadow.id = "shadow";		
 		//tip with setTimeout
 		setTimeout(function () {
 			document.body.removeChild(document.getElementById("dialog"));
+			document.body.removeChild(document.getElementById("shadow"));
 		}, 0);
 	}
 	return message;
